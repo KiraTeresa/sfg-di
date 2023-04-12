49b54d8cf9a286b396c0e2dc72415e9e8cd42474
KiraTeresa <kira.soba@gmx.de>
2023-04-12 19:44:45 +0200

    35. Dependency Injection using Spring Framework: done

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,36 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		// make GreetingServiceImpl a service (@Service)
+		// --> necessary for all the following DIs
+
+		System.out.println("------ Property");
+		// to make Property Injection work, we had to:
+		// - annotate PropertyInjectedController as a conroller (@Controller)
+		// - set @Autowired annotation to greetingService in Controller
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+		// to make Setter Injection work, we had to:
+		// - annotate SetterInjectedController as a conroller (@Controller)
+		// - autowire the setter method within that controller
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor");
+		// to make Constructor Injection work, we had to:
+		// - tell Spring Framework, that the ConstuctorInjectedController is a Spring managed component (@Controller)
+		// - no more need to add @Autowire for constructors
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
